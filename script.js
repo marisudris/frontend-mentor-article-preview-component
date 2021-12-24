9ffff7aaa759a0877f0bcfe8a8eeba4226543b58
Māris Ūdris <maris.udris.lv@gmail.com>
2021-12-24 16:13:49 +0200

    Add close-on-click-outside feature

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const toggleTip = document.querySelector('.js-toggletip');
 
 function openToggletip() {
     shareButton.setAttribute('aria-expanded', 'true');
+    shareButton.classList.add('share--active');
     toggleTip.setAttribute('aria-hidden', 'false');
 }
 
 function closeToggletip() {
     shareButton.setAttribute('aria-expanded', 'false');
+    shareButton.classList.remove('share--active');
     toggleTip.setAttribute('aria-hidden', 'true');
 }
 
-function switchToggletip() {
+function switchToggletip(evt) {
+    evt.stopPropagation();
     if (shareButton.getAttribute('aria-expanded') === 'false') {
         openToggletip();
     } else {
@@ -25,6 +28,15 @@ shareButton.addEventListener('click', switchToggletip);
 
 window.addEventListener('keyup', (evt) => {
     if (evt.key === 'Escape') {
+        closeToggletip();
+    }
+});
+
+window.addEventListener('click', (evt) => {
+    if (
+        shareButton.getAttribute('aria-expanded') === 'true' &&
+        !evt.target.closest('.toggletip')
+    ) {
         closeToggletip();
     }
 });
